6a7f7b5f3bae63f0c6cfcc08a595f375d0bc1d97
kyorohiro <kyorohiro@gmail.com>
2016-03-12 19:53:28 +0900

    add sample code

---
+++ exsample/001_reply_ip_port/server.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'dart:convert';
+
+main(List<String> args) async {
+  String host = args[0];
+  int port = int.parse(args[1]);
+  startUDPServer(host, port);
+  startTCPServer(host, port);
+}
+
+startUDPServer(String host, int port) async {
+  RawDatagramSocket socket = await RawDatagramSocket.bind(host, port, reuseAddress: true);
+  socket.listen((RawSocketEvent event) {
+    if (event == RawSocketEvent.READ) {
+      Datagram dg = socket.receive();
+      String content = "${dg.address}\n${dg.port}\n";
+      socket.send(UTF8.encode(content), dg.address, dg.port);
+    }
+  });
+}
+
+startTCPServer(String host, int port) async {
+  ServerSocket server = await ServerSocket.bind(host, port);
+  server.listen((Socket socket) {
+    String content = "${socket.remoteAddress}\n${socket.remotePort}\n";
+    socket.add(UTF8.encode(content));
+    socket.remoteAddress;
+    socket.remotePort;
+  });
+}
