8199e4619af43a7674de9539cb01d95f18e58431
kyorohiro <kyorohiro@gmail.com>
2016-03-12 20:32:20 +0900

    ..

--- exsample/001_reply_ip_port/server.dart
+++ exsample/001_reply_ip_port/server.dart
@@ -5,13 +5,13 @@ main(List<String> args) async {
   String host = args[0];
   int port = int.parse(args[1]);
   startUDPServer(host, port);
-  startTCPServer(host, port);
+  //startTCPServer(host, port);
 }
 
 startUDPServer(String host, int port) async {
   RawDatagramSocket socket = await RawDatagramSocket.bind(host, port, reuseAddress: true);
   socket.listen((RawSocketEvent event) {
-    print("--receive udp");
+    print("--receive udp ${host} ${port}");
     if (event == RawSocketEvent.READ) {
       Datagram dg = socket.receive();
       String content = "${dg.address}\n${dg.port}\n";
